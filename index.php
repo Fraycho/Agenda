8a971a9e19a87c067287e619cb994e9cbf2b29cf
Fraycho <fraycho_@hotmail.com>
2019-09-09 10:15:26 -0500

    1

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php include "includes/layout/header.php"; ?>
+
+    <div class="contenedor-barra">
+
+        <h1>Agenda de Contactos</h1>
+
+    </div>
+
+
+<?php include "includes/layout/footer.php"; ?>
