14fc3f0d3a692f924cb8d0627c60be5263ff2701
Fray <fraycho_7@hotmail.com>
2019-09-12 12:00:23 -0500

    4

---
+++ includes/functions/db.php
@@ -0,0 +1,14 @@
+
+<?php 
+
+    // Credenciales de la base de datos
+    define('DB_USUARIO', 'root');
+    define('DB_CONTRASEÑA', 'root');
+    define('DB_HOST', 'localhost');
+    define('DB_NOMBRE', 'agendaphp');
+
+    $conexion = new mysqli(DB_HOST, DB_USUARIO, DB_CONTRASEÑA, DB_NOMBRE);
+
+    echo $conexion->ping();
+
+?>
