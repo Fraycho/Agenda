5fb0199a928a30fcdb0cd02b6a65011fd0f7b66b
Fray <fraycho_7@hotmail.com>
2019-09-12 16:43:41 -0500

    4

--- includes/layout/formulario.php
+++ includes/layout/formulario.php
@@ -1,18 +1,18 @@
 
     <div class="campos">
 
-    <div class="campo">
-        <label for="nombre">Nombre:</label>
-        <input type="text" placeholder="Nombre Contacto" id="nombre">
-    </div>
-    <div class="campo">
-        <label for="empresa">Empresa:</label>
-        <input type="text" placeholder="Empresa Contacto" id="empresa">
-    </div>
-    <div class="campo">
-        <label for="telefono">Teléfono:</label>
-        <input type="tel" placeholder="Teléfono Contacto" id="telefono">
-    </div>
+        <div class="campo">
+            <label for="nombre">Nombre:</label>
+            <input type="text" placeholder="Nombre Contacto" id="nombre">
+        </div>
+        <div class="campo">
+            <label for="empresa">Empresa:</label>
+            <input type="text" placeholder="Empresa Contacto" id="empresa">
+        </div>
+        <div class="campo">
+            <label for="telefono">Teléfono:</label>
+            <input type="tel" placeholder="Teléfono Contacto" id="telefono">
+        </div>
 
     </div>
 
